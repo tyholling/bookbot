a094909eb4d5a6acb424094005175390e4707cd3
Tyler Hollingsworth <tyholling@gmail.com>
2025-12-06 03:31:26 +0000

    count words

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def get_book_text(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
 
+def count_words(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
 
 main()
