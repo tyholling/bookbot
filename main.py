c55adbce67d5df31765ffe578c9bb31a404f366b
Tyler Hollingsworth <tyholling@gmail.com>
2025-12-06 13:54:11 +0000

    refactor count words

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import count_words
+
 def get_book_text(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
-
-def count_words(text):
-    return len(text.split())
 
 def main():
     text = get_book_text("books/frankenstein.txt")
