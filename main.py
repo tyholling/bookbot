d344370b42d8cb1958214fc1e2538056af014f0f
Tyler Hollingsworth <tyholling@gmail.com>
2025-12-08 17:15:37 +0000

    add report

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+
+    path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {path}...")
+
+    text = get_book_text(path)
     num_words = stats.count_words(text)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+
     symbols = stats.count_symbols(text)
-    print(symbols)
+    sorted_symbols = sorted(symbols.items(), reverse=True, key=lambda x: x[1])
+    print("--------- Character Count -------")
+    for item in sorted_symbols:
+        print(f"{item[0]}: {item[1]}")
+
+    print("============= END ===============")
 
 main()
